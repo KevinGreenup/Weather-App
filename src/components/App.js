982a0c98d3f0df21d46172d68185f9660714de4b
Kevin Greenup <kevin.greenup@hotmail.co.uk>
2024-02-17 18:57:03 +0000

    fixed bug with app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import '../styles/App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="prop">
-      <h1>Weather App</h1>
-    </div>
-  );
-}
+const App = () => <h1>Hello World</h1>;
+
+// The above utilises an implicit return, which is a feature of arrow-functions.
+// It is the same as this:
+// const App = () => {
+//  return <h1>Hello World</h1>;
+// }
+// or this:
+// function App() {
+//   return <h1>Hello World</h1>;   
+// }
 
 export default App;
 
